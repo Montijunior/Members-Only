1070bea4bfe4d167ee5e88e3f8c311e5e924a547
Monti junior <montiabouou@gmail.com>
2024-09-08 17:50:33 -0800

    Code refactoring

--- routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const authRouter = Router();
-const AuthController = require("../controllers/AuthController");
-
-// GET signup page
-authRouter.get("/signup", AuthController.user_signup_get);
-
-module.exports = authRouter;
