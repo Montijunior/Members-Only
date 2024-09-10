3829d367b0daef9f4ed6e708a65ad5928058875d
Monti junior <montiabouou@gmail.com>
2024-09-10 11:14:17 -0800

    Added pg-connect-simple module

--- app.js
+++ app.js
@@ -1,63 +1,33 @@
 require("dotenv").config();
-const exp = require("constants");
 const express = require("express");
 const session = require("express-session");
-const passport = require("./passport");
+const pgSession = require("connect-pg-simple")(session);
 const path = require("path");
-const userRoutes = require("./routes/userRoutes");
-const authRoutes = require("./routes/authRoutes");
+const pool = require("./models/pool");
+const passport = require("passport");
 
 const app = express();
-const secret = process.env.SECRET_KEY;
 
 app.set("views", path.join("views"));
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 app.use(
   session({
-    secret: secret,
+    store: new pgSession({
+      pool,
+      tableName: "session",
+    }),
+    secret: process.env.SECRET_KEY,
     resave: false,
     saveUninitialized: false,
-    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    },
   })
 );
-
-// passport
-app.use(passport.session());
-// all controllers and viewers router: user
-app.use((req, res, next) => {
-  app.locals.currentUser = req.user;
-  next();
-});
-
-// App routes
-app.use("/", userRoutes);
-app.use("/auth", authRoutes);
-
-app.set(passport);
-
-// Login route
-app.post(
-  "/auth/login",
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/auth/login",
-  })
+app.listen(process.env.PORT, () =>
+  console.log(
+    `Members Only Application listening on localhost:${process.env.PORT}`
+  )
 );
-
-// Logout route
-app.get("/auth/logout", (req, res) => {
-  req.user((err) => {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/");
-  });
-});
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Members Only Application running on localhost:${port}`);
-});
-
-module.exports = { app };
