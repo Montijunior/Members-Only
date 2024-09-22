6e5bea3be809ec8430bc4747b1fea2c86c344799
Monti junior <montiabouou@gmail.com>
2024-09-21 17:21:31 -0800

    added express.json() middleware to app

--- app.js
+++ app.js
@@ -5,13 +5,16 @@ const pgSession = require("connect-pg-simple")(session);
 const path = require("path");
 const pool = require("./models/pool");
 const passport = require("passport");
+const userRoutes = require("./routes/userRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.set("views", path.join("views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(
   session({
     store: new pgSession({
@@ -26,6 +29,16 @@ app.use(
     },
   })
 );
+app.use(passport.session());
+app.use((req, res, next) => {
+  app.locals.currentUser = req.user;
+  next();
+});
+
+// Routes
+app.use("/", userRoutes);
+app.use("/auth", authRoutes);
+
 app.listen(process.env.PORT, () =>
   console.log(
     `Members Only Application listening on localhost:${process.env.PORT}`
