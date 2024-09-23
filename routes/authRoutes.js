70f34a7a80fc20f46576f05f67101489f41206e6
Monti junior <montiabouou@gmail.com>
2024-09-22 17:01:28 -0800

    added login and logout routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,4 +10,10 @@ authRouter.post("/signup", authController.post_sign_up_form);
 // GET sign in form
 authRouter.get("/login", authController.get_sign_in_form);
 
+// POST sign in
+authRouter.post("/login", authController.post_sign_in_form);
+
+// GET logout
+authRouter.get("/logout", authController.get_logout);
+
 module.exports = authRouter;
