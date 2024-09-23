ee97f96f91fc53e570df23e77741b76c2121bc6e
Monti junior <montiabouou@gmail.com>
2024-09-22 19:05:56 -0800

    added controllers for message routes

--- controllers/UserControllers.js
+++ controllers/UserControllers.js
@@ -1,5 +1,6 @@
 const DB = require("../models/queries");
-const { body, validationResult } = require("express-validator");
+const { validationResult } = require("express-validator");
+const { messageFormValidations } = require("../validations/validation");
 
 // HOME PAGE
 exports.home_page_get = async (req, res) => {
@@ -18,5 +19,23 @@ exports.get_new_message_form = (req, res) => {
   if (!req.isAuthenticated()) {
     return res.redirect("/auth/login");
   }
-  return res.render("messageForm", { title: "New Message" });
+  return res.render("messageForm", { title: "New Message", errors: [] });
 };
+
+// POST new message
+exports.post_new_message_form = [
+  messageFormValidations,
+  async (req, res) => {
+    const { title, text } = req.body;
+    const { id } = req.params;
+    const results = validationResult(req);
+    if (!results.isEmpty()) {
+      return res.render("messageForm", {
+        title: "New Message",
+        errors: results.array(),
+      });
+    }
+    await DB.insertMessage(id, title, text);
+    return res.redirect("/");
+  },
+];
