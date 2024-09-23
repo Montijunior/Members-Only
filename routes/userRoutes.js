c8e2f27148446096bb3ac0955150b2daa507eae4
Monti junior <montiabouou@gmail.com>
2024-09-22 19:02:28 -0800

    added routes for message get & post

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,4 +8,7 @@ userRouter.get("/", userController.home_page_get);
 // GET new message form (if user is authenticated)
 userRouter.get("/message/:id/new", userController.get_new_message_form);
 
+// POST new message
+userRouter.post("/message/:id/new", userController.post_new_message_form);
+
 module.exports = userRouter;
